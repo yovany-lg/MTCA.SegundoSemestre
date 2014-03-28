f0c4b905e63e15011634cb8b021bc9db6b1bf23c
yovany.lg Posgrado <yovany.lg@gmail.com>
2014-03-28 15:19:08 -0600

    Avances Perceptron

--- ANN/Perceptron/main.cpp
+++ ANN/Perceptron/main.cpp
@@ -9,109 +9,126 @@
 #include <fstream>
 #include <iostream>
 #include <cstring>
+#include <string>
 
+
+struct charDotMatrix{
+    char** dotMatrix;
+};
+
+struct rowCharsSelection{
+    int row;
+    int charWidth;    //{width, height}
+    int charHeight;
+    struct charDotMatrix *charVector;
+};
 using namespace std;
-unsigned char* readBMP(string);
+void readBMP(string, char**);
 void bmp_vector(string);
+void retrieveRowSelection(rowCharsSelection row, char **dotMatrix);
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     
     string imageName = "",fullPath ="images/";
-    unsigned char* vector; 
-    cout << "Introduce el nombre del archivo *.bmp en la ruta ../images/: ";
+    char** dotMatrix; //Para generar la matriz dinámica
+    rowCharsSelection rowSelection[2] = {
+        {4,32,32,new charDotMatrix[16]},
+        {5,32,32,new charDotMatrix[16]}
+    };
+    
+    cout << "Introduce el nombre del archivo fuente (*.bmp) en la ruta ../images/: ";
     cin >> imageName;
-    fullPath+= imageName+".bmp";
-    vector = readBMP(fullPath);
-    cout << strlen((char*)vector);
+    fullPath+= imageName+".bmp";    //Ruta completa del archivo
+    
+    readBMP(fullPath, dotMatrix); //Obtener la imagen en una matriz de puntos
+    retrieveRowSelection(rowSelection[0],dotMatrix);
+    //Solo para ver si funciona esta cosa :P
+    for(int i = 0; i<32*8; i++)
+    {
+        for(int j=0;j<32*5;j++)
+        {
+            cout << dotMatrix[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
 
-void bmp_vector(string filePath){
-    ifstream fin;
-    fin.open(filePath.c_str(),ios_base::in|ios_base::binary);
-    char header[54];
-    if (!fin) // open attempt failed
+void retrieveRowSelection(rowCharsSelection row, char **dotMatrix){
+    int matrixRowInit = row.charHeight*(row.row-1); //Equivalente en la fila de la matriz
+    int matrixRowEnd = matrixRowInit + row.charHeight;//Fila final de la búsqueda
+    char matrix[32][32];
+    for(int i = matrixRowInit; i < matrixRowEnd ; i++)
     {
-        cout << "No se pudo leer el archivo";
+        for(int x = 0; x<15;x++)
+        {
+            ofstream fileOutput;
+            string filename = "fontFiles/char.txt";
+            fileOutput.open(filename.c_str());
+            int ref = 32*x;
+            string content= "";
+            for(int j=ref; j<(ref+32);j++)
+                 content += dotMatrix[i][j];
+            fileOutput<<content<<endl;
+            
+        }
     }
-    fin.read(header,54); 
-/*    
-    char ch;
-
-    while (fin.get()) // read character from file and
-        cout << ch; // write it to screen
-    
-    cout << "Done\n";
-    fin.close();
-*/    
-//    return 0;    
 }
 
-unsigned char* readBMP(string filename)
+
+
+void readBMP(string filename, char** dotMatrix)
 {
-    int i;
     FILE* f = fopen(filename.c_str(), "rb");
+    unsigned char header[54];
     
     if(f == NULL)
     {
         cout << "No se pudo abrir el archivo";
-        return 0;
+        return;
     }
-    unsigned char header[54];
 
-    fread(header, sizeof(unsigned char), 54, f); // read the 54-byte header
-
-    // extract image height and width from header
+    //Extraer el header con la información del archivo
+    fread(header, sizeof(unsigned char), 54, f);
+    
+    //Obtener largo y ancho de la imagen
     int width = *(int*)&header[18];
     int height = *(int*)&header[22];
-
+    //Generar la matríz dinámica
+    *dotMatrix = new char[width];
+    for(int x = 0; x<width;x++)
+        dotMatrix[x] = new char[height];
+    
     cout << endl;
     cout << "  Name: " << filename << endl;
     cout << " Width: " << width << endl;
     cout << "Height: " << height << endl;
     
+    //Tamaño de datos a leer considerando el tamaño de la información del
+    //color del pixel
     int row_padded = (width*3 + 3) & (~3);
     unsigned char* data = new unsigned char[row_padded];
     unsigned char tmp;
-
-    for(int i = 0; i < 32; i++)
-    {
+    
+    //Recorrer la matriz de pixeles
+    for(int i = 0; i < width; i++)
+    {           
+        //Leer un pixel
         fread(data, sizeof(unsigned char), row_padded, f);
-        for(int j = 0; j < 160*3; j += 3)
+        for(int j = 0; j < height*3; j += 3)
         {
-            // Convert (B, G, R) to (R, G, B)
+            // Convertir de (B, G, R) a (R, G, B)
             tmp = data[j];
             data[j] = data[j+2];
             data[j+2] = tmp;
+            //Validar si es un pixel vacío o relleno
             if(data[j] > 0 and data[j+1] > 0 and data[j+2] > 0)
-                cout<<' ';
+                dotMatrix[width-i-1][(int)j/3] = ' ';
             else
-                cout<<'*';
-            //cout << "R: "<< (int)data[j] << " G: " << (int)data[j+1]<< " B: " << (int)data[j+2]<< endl;
+                dotMatrix[width-i-1][(int)j/3] = '*';
         }
         cout<<endl;
     }
-
     fclose(f);
-    return data;
-/*
-
-    int size = 3 * width * height;
-    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
-    fclose(f);
-
-    for(i = 0; i < size; i += 3)
-    {
-            unsigned char tmp = data[i];
-            data[i] = data[i+2];
-            data[i+2] = tmp;
-    }
-
-    return data;
- */
 }
 
