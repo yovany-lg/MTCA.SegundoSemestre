78c72f1ab1ac23fd4592f00e9d28b465ebba1751
yovany.lg Posgrado <yovany.lg@gmail.com>
2014-05-13 14:20:04 -0500

    Avances Back Propagation
    Feedforward

--- ANN/backPropagation/main.cpp
+++ ANN/backPropagation/main.cpp
@@ -9,10 +9,12 @@
 #include <cstdlib> // rand(), srand() prototypes
 #include <ctime> // time() prototype
 #include <iostream>
+#include <cmath>
 
 struct Layer{
     int neurons;
-    float **weightMatrix;
+    float **weightMatrix;   //En la capa de entrada el apuntador esta a nulo
+    float **outputVector;   //En la capa de entrada se apunta al vector de entrada
     Layer *nextLayer, *prevLayer;
 };
 
@@ -25,15 +27,22 @@ using namespace std;
 
 void weightMatrixInit(int neurons, int inputs, float ***weightMatrix);
 void addLayer(int neurons, int inputs, Layer** layersList);
-void initialize(int layers,int neurons[],int inputs,Layer** layersList);
+void initialize(int layers,int neurons[],Layer** layersList);
+void addInputLayer(int neurons, Layer** layersList);
+void feedforward(Pattern **trainingPattern, Layer** layersList);
+void getOutputVector(Layer** currentLayer);
+float getLocalField(float **inputVector, float **weightVector, int inputs);
+float getFunctionSignal(float localField);
+void getInputPattern(Pattern **trainingPattern, Layer** inputLayer);
+void trainingPatternsIterations(Layer** layersList, Pattern **trainingPatterns, int nPatterns);
 /*
  * 
  */
 int main(int argc, char** argv) {
-    Layer* layersList = NULL;     // layersList -> Oculta <-> Salida -> NULL
+    Layer* layersList = NULL;     // layersList: Entrada <-> Oculta <-> Salida -> NULL
     
-    int inputs = 5, layers = 2;
-    int neurons[2] = {5,6}; // Formato Vector de neuronas [nSalida,nOculta1,...]
+    int layers = 3;
+    int neurons[3] = {5,6,5}; // Formato Vector de neuronas [nSalida,nOculta1,nEntradas,...]
     Pattern p1 = {{0.01,0.02,0.02,0.02,0.4},{0.1699,0.2762,0.2346,0.0854,0.3524}}, 
             p2 = {{0.01,0.02,0.02,0.02,1.0},{0.2599,0.3591,0.2397,0.1065,0.6190}}, 
             p3 = {{0.01,0.02,0.02,0.04,0.4},{0.3082,0.3783,0.2973,0.1846,0.3825}},
@@ -46,30 +55,35 @@ int main(int argc, char** argv) {
             p10 = {{0.02,0.04,0.04,0.08,1.0},{0.6158,0.8271,0.5982,0.5275,1}};
     
     Pattern * trainingPatterns[10] = {&p1,&p2,&p3,&p4,&p5,&p6,&p7,&p8,&p9,&p10};
-    initialize(layers,neurons,inputs,&layersList);
+    initialize(layers,neurons,&layersList);
+    trainingPatternsIterations(&layersList,trainingPatterns,10);
+    
     return 0;
 }
 
 //-- Inicializar Capas de la Red
-void initialize(int layers,int neurons[],int inputs,Layer** layersList){
-    int prevInputs;
-    // layersList -> Oculta <-> Salida -> NULL
-    // Formato Vector de neuronas [nSalida,nOculta1,...]
+void initialize(int layers,int neurons[],Layer** layersList){
+    // Formato Vector de neuronas [nSalida,nOculta1,nEntradas,...]
     for(int i = 0; i < layers; i++) 
     {
-        if(i == (layers - 1))   //La primer capa oculta
-            prevInputs = inputs;    //Toma las entradas del vector de entrada
-        else
-            prevInputs = neurons[i+1]; //Las entradas de la capa actual son las neuronas de la capa anterior (oculta)
-        addLayer(neurons[i],prevInputs,layersList);
+        if (i == (layers-1))    //Para la capa de entrada
+        {
+            addInputLayer(neurons[i],layersList);
+        }else{
+            addLayer(neurons[i],neurons[i+1],layersList);
+        }
     }
 }
 
-void addLayer(int neurons, int inputs, Layer** layersList)
-{
+void addLayer(int neurons, int inputs, Layer** layersList){
     Layer *newLayer = new Layer;
+    float *outputVector = new float[neurons];
+    
     newLayer->neurons = neurons;
     weightMatrixInit(neurons, inputs, &(newLayer->weightMatrix));
+    newLayer->outputVector = &outputVector;    //Vector de salida de las neuronas
+    
+    //Orden en la lista de Capas
     newLayer->prevLayer = NULL;
     newLayer->nextLayer = *layersList;
     if(*layersList != NULL)
@@ -77,11 +91,26 @@ void addLayer(int neurons, int inputs, Layer** layersList)
     *layersList = newLayer;
 }
 
+void addInputLayer(int neurons, Layer** layersList){
+    Layer *newLayer = new Layer;
+    float *outputVector = new float[neurons];
+    
+    newLayer->neurons = neurons;
+    newLayer->weightMatrix = NULL;   //En capa de entrada no se necesita
+    newLayer->outputVector = &outputVector;    //El vector de salida tomará la dirección del vector del patrón de entrada
+    
+    //Orden en la lista de Capas
+    newLayer->prevLayer = NULL;
+    newLayer->nextLayer = *layersList;
+    if(*layersList != NULL)
+        (*layersList)->prevLayer = newLayer;
+    *layersList = newLayer;
+}
+
 //-- Inicializar Matriz de pesos
-void weightMatrixInit(int neurons, int inputs, float ***weightMatrix)
-{
+void weightMatrixInit(int neurons, int inputs, float ***weightMatrix){
     float randStep = (float)2/100; //Pasos discretos
-    srand(time(0)); // seed random-number generator
+    srand(time(NULL)); // seed random-number generator
     
     *weightMatrix = new float*[neurons]; 
     for(int i = 0 ; i<neurons;i++)
@@ -90,12 +119,51 @@ void weightMatrixInit(int neurons, int inputs, float ***weightMatrix)
         for(int j = 0; j<(inputs+1);j++)
         {
             float randValue = (rand() % 100)*randStep - 1;
-            cout<<"Peso Aleatorio>"<<randValue;
             (*weightMatrix)[i][j] = randValue;   //Inicializacion de los pesos y el bias        
         }
     }
 }
 
-void trainNN(int inputs,Layer** layersList, Pattern **trainingPatterns){
+void trainingPatternsIterations(Layer** layersList, Pattern **trainingPatterns, int nPatterns){
+    for(int i = 0; i< nPatterns;i++){
+        feedforward(&(trainingPatterns[i]),layersList);
+    }
+}
+
+void feedforward(Pattern **trainingPattern, Layer** layersList){
+    Layer *currentLayer = *layersList;  //La primer capa (Capa de Entrada)... layersList: Entrada <-> Oculta <-> Salida -> NULL
+    getInputPattern(trainingPattern,&currentLayer);
     
+    while(currentLayer->nextLayer != NULL){ //Mientras no halla más capas
+        currentLayer = currentLayer->nextLayer;
+        getOutputVector(&currentLayer);
+    }
+}
+
+void getInputPattern(Pattern **trainingPattern, Layer** inputLayer){
+    for(int i = 0; i < (*inputLayer)->neurons; i++)
+        (*(*inputLayer)->outputVector)[i] = (*trainingPattern)->inputs[i];
+}
+
+void getOutputVector(Layer** currentLayer){
+    Layer *prevLayer = (*currentLayer)->prevLayer;//Para obtener inf. de la capa anterior
+    float localField = 0;
+    for(int i = 0; i < (*currentLayer)->neurons; i++){  //Obtener la salida por cada neurona
+        //Weighted Input Signal
+        localField = getLocalField(prevLayer->outputVector,&((*currentLayer)->weightMatrix[i]),prevLayer->neurons);
+        (*(*currentLayer)->outputVector)[i] = getFunctionSignal(localField);
+    }
+}
+
+float getLocalField(float **inputVector, float **weightVector, int inputs){
+    float y_in = 1*(*weightVector)[0]; //Se calcula la aportación del BIAS
+    for(int i = 0; i < inputs; i++){ 
+        y_in += ((*inputVector)[i]) * ((*weightVector)[i+1]);  //Desplazado para no considerar al BIAS
+    }
+    return y_in;
+}
+
+//-- Aplicando la Función de Activación
+float getFunctionSignal(float localField){
+    return 1/(1+exp(-localField));
 }
--- ANN/backPropagation/nbproject/Makefile-variables.mk
+++ ANN/backPropagation/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=backpropagation
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/backpropagation
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/backpropagation
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=backpropagation.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/backpropagation.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/backpropagation.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=backpropagation
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/backpropagation
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/backpropagation
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=backpropagation.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/backpropagation.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/backpropagation.tar
 #
 # include compiler specific variables
 #
